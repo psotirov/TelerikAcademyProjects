0d3bd36bfe78ee4d0a077b1a44d57833dfa1eca1
Pavel Sotirov <me@psotirov.cloudvps.bg>
2014-01-06 19:45:33 +0200

    remove old data

--- OOP Exam/HTML Renderer/IElementFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace HTMLRenderer
-{
-    public interface IElementFactory
-    {
-        IElement CreateElement(string name);
-        IElement CreateElement(string name, string content);
-        ITable CreateTable(int rows, int cols);
-    }
-}
